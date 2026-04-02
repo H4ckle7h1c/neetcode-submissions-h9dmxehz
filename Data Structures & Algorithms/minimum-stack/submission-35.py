b532023204ce0a7fbb5ba1b222240550cb0b0b37
Ludovic Peyronnet <56484322+H4ckle7h1c@users.noreply.github.com>
2026-04-02 11:13:13 +0700

    Add: minimum-stack - submission-35

---
+++ Data Structures & Algorithms/minimum-stack/submission-35.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, val, min):
+        self.val = val
+        self.min = min
+        self.prev = None
+class MinStack:
+
+    def __init__(self):
+        self.head = None
+
+    def push(self, val: int) -> None:
+        if self.head == None:
+            self.head = Node(val, val)
+            return 
+        
+        new_head = Node(val, val)
+
+        if self.head.min < val:
+            new_head.min = self.head.min
+        
+        new_head.prev = self.head
+        self.head = new_head
+
+    def pop(self) -> None:
+        if self.head is not None:
+            self.head = self.head.prev
+
+        
+
+    def top(self) -> int:
+        if self.head == None:
+            return -1 
+        
+        return self.head.val
+        
+
+    def getMin(self) -> int:
+        return self.head.min
