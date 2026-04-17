fbcc51baf49b33334d8a60e8078aa2882dc704c3
Ludovic Peyronnet <56484322+H4ckle7h1c@users.noreply.github.com>
2026-04-17 20:00:24 +0700

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Step 1 - find a candidate row that could contain the target
+        for row in matrix:
+
+            # Step 2 - We found a candidate row that could contain the target
+            if row[0] <= target and target <= row[-1]:
+                l, r = 0, len(row) - 1 
+                while l <= r:
+                    mid = (r - l) // 2 + l 
+
+                    if row[mid] == target:
+                        return True
+
+                    if row[mid] < target: 
+                        l = mid + 1
+                        continue
+                    
+                    if row[mid] > target: 
+                        r = mid - 1
+                        continue
+                
+                return False
+        
+        return False
+                
+        
+        
+
+        
